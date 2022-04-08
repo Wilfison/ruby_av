f0a98ab4666941dc7019314df91b3cf783149f0f
wilfison <wilfisonbatista@gmail.com>
2022-04-08 12:00:18 -0300

    fix comments

--- examples/concat_videos.rb
+++ examples/concat_videos.rb
@@ -6,11 +6,17 @@ require_relative "../lib/ruby_av"
 output_path = "#{Dir.pwd}/tmp"
 output_file = "#{output_path}/output.mp4"
 
+# starts a new encoder instance with the output file name
 RubyAv::Encoder.run(output_file) do |enc|
+  # add the first input file
   enc.add_input("#{output_path}/input.mp4")
+  # add the second input file
   enc.add_input("#{output_path}/input.mp4")
 
+  # apply the concatenate effect
   enc.add_filter_complex :concat
 
+  # run the command in ffmpeg
+  # and validate if the output file was generated
   enc.transcode(validate: true)
 end
--- examples/crop_trim.rb
+++ examples/crop_trim.rb
@@ -6,11 +6,16 @@ require_relative "../lib/ruby_av"
 output_path = "#{Dir.pwd}/tmp"
 output_file = "#{output_path}/croped_output.mp4"
 
+# Cut/Trim a video file
 RubyAv::Encoder.run(output_file) do |enc|
+  # add the input file
   enc.add_input("#{output_path}/input.mp4")
 
+  # add trim filter and set start and end time
   # time format: HH:MM:SS.MILLISECONDS
   enc.add_filter_complex :trim, start: "00:00:00", end: "00:00:05"
 
+  # run the command in ffmpeg
+  # and validate if the output file was generated
   enc.transcode(validate: true)
 end
