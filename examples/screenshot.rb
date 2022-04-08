28f536f60fadb6dbbe3b1aa3ff9a417dabd456df
wilfison <wilfisonbatista@gmail.com>
2022-04-08 16:08:11 -0300

    fix formatting

--- examples/screenshot.rb
+++ examples/screenshot.rb
@@ -15,11 +15,7 @@ media.screenshot("#{output_path}/output_3s.png", seek_time: "00:00:03")
 
 # capture 1 frame on 5s
 # with custom resolution
-media.screenshot(
-  "#{output_path}/output_#{opts1[:resolution]}.png",
-  resolution: "480x640",
-  seek_time: "00:00:05"
-)
+media.screenshot("#{output_path}/output_5s.png", resolution: "480x640", seek_time: 5)
 
 # Screenshot/Thumbnail every 10 seconds
 # read more about in this post
