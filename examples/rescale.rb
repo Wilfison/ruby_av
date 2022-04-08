e1279e0ea003a19fdada4c27d845c0060a62fda3
wilfison <wilfisonbatista@gmail.com>
2022-04-08 14:55:35 -0300

    create a Rescale example

---
+++ examples/rescale.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative "../lib/ruby_av"
+
+output_path = "#{Dir.pwd}/tmp"
+
+# read more about Scaling
+# https://trac.ffmpeg.org/wiki/Scaling
+
+# Simple Rescaling
+RubyAv::Encoder.run("#{output_path}/output_hd.mp4") do |enc|
+  # add the input file
+  enc.add_input("#{output_path}/input.mp4")
+
+  # set video filter scale
+  enc.add_other_option "-vf", "scale=720:1080,setsar=1:1"
+
+  # run the command in ffmpeg
+  # and validate if the output file was generated
+  enc.transcode(validate: true)
+end
+
+# Keeping the Aspect Ratio
+RubyAv::Encoder.run("#{output_path}/output_sd.mp4") do |enc|
+  # add the input file
+  enc.add_input("#{output_path}/input.mp4")
+
+  # If we'd like to keep the aspect ratio, we need to specify only one component,
+  # either width or height, and set the other component to -1.
+  enc.add_other_option "-vf", "scale=640:-1"
+
+  # run the command in ffmpeg
+  # and validate if the output file was generated
+  enc.transcode(validate: true)
+end
